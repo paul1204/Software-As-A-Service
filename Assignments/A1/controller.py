34c9a00e29e17c429345e99a046e9b6cbb2b68ea
Paramveer <singhp35@mymail.vcu.edu>
2021-09-12 19:17:58 -0400

    Created Form

---
+++ Assignments/A1/controller.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, InputRequired, Length
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name? (First, Last)', validators=[DataRequired()])
+    studentID = StringField('Enter VID', [InputRequired(), Length(min=9,max=9,message='VID is 9 Digits')])
+    courseID = StringField('Enter The Course Number You Would Like To Join', [InputRequired(), Length(min=10,max=10,message='Enter a valid ID')])      
+
+    submit = SubmitField('Submit')
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
