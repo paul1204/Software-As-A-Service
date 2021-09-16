dbb8444ea466708ce2ad2e24a4704cbf7ba88edb
Paramveer <singhp35@mymail.vcu.edu>
2021-09-16 12:13:32 -0400

    Changed Nav Bar Color

--- Assignments/A1/controller.py
+++ Assignments/A1/controller.py
@@ -25,6 +25,7 @@ def internal_server_error(e):
 def dropdown():
     options = ['A', 'B', 'C', 'D']
    return render_template('options.html', options=options)
+https://stackoverflow.com/questions/46047658/how-to-add-a-drop-down-with-valuespre-defined-in-flask-app-models
 
 """
 """
@@ -39,13 +40,13 @@ class NameForm(FlaskForm):
     name = StringField('What is your name? (First, Last)', validators=[DataRequired()])
     studentID = StringField('Enter VID', [InputRequired(), Length(min=9,max=9,message='VID is 9 Digits')])
     email = StringField('Enter Email Address', validators=[DataRequired()])
+    dropList = ['','Desired Section At Capacity', 'Prerequiste Not Satisfied', 'Other']
+    s = SelectField('Reason', choices=dropList, default=1)
     #CMSC 425; 4 Characters for Class Type, 1 space, 3 digit number = 8 min and max
     courseID = StringField('Enter The Course Number You Would Like To Join', [InputRequired(), Length(min=8,max=8,message='Enter a Valid ID')])      
     sectionNum = StringField('Enter Section Number', [InputRequired(), Length(min=3,max=3,message='Enter a Valid Section Number')])
     #Course Reference Number
     crn = StringField('Enter Course Reference Numbers', [InputRequired(), Length(min=5,max=5,message='Enter a Valid Course Reference Number')])
-    dropList = ['','Desired Section At Capacity', 'Prerequiste Not Satisfied', 'Other']
-    s = SelectField('Reason', choices=dropList, default=1)
     
     submit = SubmitField('Submit')
 
