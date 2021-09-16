544282287b606990e02087c874e9acd6dc2db0b5
Paramveer <singhp35@mymail.vcu.edu>
2021-09-16 13:07:01 -0400

    Added Thank You Re Direct Page

--- Assignments/A1/controller.py
+++ Assignments/A1/controller.py
@@ -56,6 +56,7 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
-        return redirect(url_for('index'))
+        #return redirect(url_for('index'))
+        return render_template('thanks.html', form=form, name=session.get('name'))
     return render_template('index.html', form=form, name=session.get('name'))
 
